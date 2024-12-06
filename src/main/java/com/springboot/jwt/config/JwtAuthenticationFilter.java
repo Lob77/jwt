e0936fc494f2d19d3e274e7f92c77d105bd31b3f
Lob77 <uueel1224@gmail.com>
2024-12-06 15:25:51 +0900

    [Add]Jwt 토큰 인증필터 추가

---
+++ src/main/java/com/springboot/jwt/config/JwtAuthenticationFilter.java
@@ -0,0 +1,30 @@
+package com.springboot.jwt.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends GenericFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
+
+        if(token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+}
