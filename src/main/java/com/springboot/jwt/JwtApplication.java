4b52873c4f58f99c0e102807672735d2d694a5cc
Lob77 <uueel1224@gmail.com>
2024-12-06 15:23:49 +0900

    [Add]암호화 모듈 객체 추가

--- src/main/java/com/springboot/jwt/JwtApplication.java
+++ src/main/java/com/springboot/jwt/JwtApplication.java
@@ -3,12 +3,19 @@ package com.springboot.jwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class JwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
