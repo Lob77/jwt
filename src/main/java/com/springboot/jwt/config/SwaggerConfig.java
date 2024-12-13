dd9ad9fe9bfcee57fc8ee1a0deff88574af9887f
Lob77 <uueel1224@gmail.com>
2024-12-13 20:30:27 +0900

    [Add]Swagger Authorize 테스트를 위해 설정 추가

--- src/main/java/com/springboot/jwt/config/SwaggerConfig.java
+++ src/main/java/com/springboot/jwt/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.springboot.jwt.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +14,17 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(info());
+        return new OpenAPI()
+                .info(info())
+                .addSecurityItem(new SecurityRequirement().addList("JWT")) // 보안 요구 사항 추가 (JWT 인증)
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer") // 인증 방식: bearer
+                            .bearerFormat("JWT") // JWT 토큰 형식
+                            .in(SecurityScheme.In.HEADER) // 인증 정보는 HTTP 헤더에 포함
+                            .name("Authorization")
+                ));
     }
 
     @Bean
